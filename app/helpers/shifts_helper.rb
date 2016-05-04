78cf9547986f3b79f9f403b1e7d2c8fdad07d2bb
admin <admin@iMac-2.local>
2016-05-04 15:01:12 -0400

    gsgs

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -1,34 +1,5 @@
 module ShiftsHelper
   def find_shifts(guard)
 
-    @all_shifts = Shift.where(guard_id: guard.id)
-    @w ="test"
-    @week = Hash.new
-    @all_shifts.each do |shift|
-
-      time = shift.datetime.strftime('%H:%M')
-      case(shift.datetime.strftime('%A'))
-      when 'Monday'
-        if shift.on_shift == true
-          @week['monday_on'] = time
-        else
-          @week['monday_off'] = time
-        end
-      when 'Saturday'
-        if shift.on_shift == true
-          @week['saturday_on'] = time
-        else
-          @week['saturday_off'] = time
-        end
-        when 'Wednesday'
-          if shift.on_shift == true
-        @week['tuesday_on'] = time
-        else
-        @week['tuesday_off'] = time
-      end
-      end
-
-    end
-   
   end
 end
