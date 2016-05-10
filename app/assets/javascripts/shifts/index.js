368bffc7df8787c4a1f580f550d47cb61ff4363c
admin <admin@iMac-2.local>
2016-05-10 18:15:38 -0400

    Diferencia de horas tomando valor que se entra por teclado

--- app/assets/javascripts/shifts/index.js
+++ app/assets/javascripts/shifts/index.js
@@ -35,27 +35,35 @@ function calculate_date_hours(){
   })
 }
 function calculate_hours(day,index){
-  var h_on,m_on,s_on,h_off,m_off,s_off,
+  var h_on = 00,m_on =00,h_off= 00,m_off =00,
       day_on = new Date(),
       day_off = new Date(),
-      text_on = $("#tr-"+index+" #"+day+"-on").text(),
-      text_off = $("#tr-"+index+" #"+day+"-off").text();
-    //  text_on = $("#tr-"+index+" #"+day+"-on input") ? $("#tr-"+index+" #"+day+"-on input").val() : $("#tr-"+index+" #"+day+"-on").text(),
-    //  text_off =  $("#tr-"+index+" #"+day+"-off input") ? $("#tr-"+index+" #"+day+"-off input").val() : $("#tr-"+index+" #"+day+"-off").text();
+  textOn = ($("#tr-"+index+" input[name='time"+day+"-on']").length > 0 ) ? $("#tr-"+index+" input[name='time"+day+"-on']").val() : $("#tr-"+index+" #"+day+"-on").text(),
+  textOff = ($("#tr-"+index+" input[name='time"+day+"-off']").length > 0) ? $("#tr-"+index+" input[name='time"+day+"-off']").val() : $("#tr-"+index+" #"+day+"-off").text();
+//  alert($("#tr-"+index+" input[name='time"+day+"-on']").val() );
+  //alert(textOff);
+  /*
+  textOn = $("#tr-"+index+" #"+day+"-on").text(),
+  textOff = $("#tr-"+index+" #"+day+"-off").text();
+  */
 
-  //   text_on = ''+text_on+''
-  //   text_off = ''+text_off+''
-     h_on = text_on.substr(0, 2);
-    m_on = text_on.substr(3, 2);
-    h_off = text_off.substr(0, 2);
-    m_off = text_off.substr(3, 2);
+   if (textOn != " ") {
+     h_on = textOn.substr(0, 2);
+     m_on = textOn.substr(3, 2);
+   }
+   if (textOff != " ") {
+     h_off = textOff.substr(0, 2);
+     m_off = textOff.substr(3, 2);
+   }
+
 
     day_on.setHours(h_on, m_on, 0);
     day_off.setHours(h_off, m_off, 0);
     var diff = day_off - day_on;
-//diff_time(day_on, day_off)
+    //diff_time(day_on, day_off)
+//convert_time_msecond(diff)
     $("#tr-"+index+" #"+day+"-total").text(convert_time_msecond(diff));
-    return diff;
+     return diff;
 
 }
 
@@ -79,4 +87,7 @@ function convert_time_msecond(msecond){
     var seconds = Math.floor(msecond / 1000 );
 
     return (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) ;
+}
+function trim(string){
+    return string.replace(/^\s*|\s*$/g, '');
 }
