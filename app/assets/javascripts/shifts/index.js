3849b78c72e74e5c4b4ac6b4695d79494780bd1f
admin <admin@iMac-2.local>
2016-05-10 16:28:14 -0400

    Resta de las horas por dia y suma total por guard

--- app/assets/javascripts/shifts/index.js
+++ app/assets/javascripts/shifts/index.js
@@ -1,43 +1,82 @@
 $(document).ready(function(){
-
+/*
+  $('.time').each(function(){
+    self.timepicker({
+             minuteStep: 1,
+             template: 'modal',
+             appendWidgetTo: 'body',
+             showSeconds: true,
+             showMeridian: false,
+             defaultTime: false
+         });
+  })*/
 });
+
+/*function convertTo24Hour(time) {
+    var hours = parseInt(time.substr(0, 2));
+    if(time.indexOf('am') != -1 && hours == 12) {
+        time = time.replace('12', '0');
+    }
+    if(time.indexOf('pm')  != -1 && hours < 12) {
+        time = time.replace(hours, (hours + 12));
+    }
+    return time.replace(/(am|pm)/, '');
+}*/
 function calculate_date_hours(){
+  var  total_week, i;
   $("#listing-shifts tbody tr").each(function (index)
-  {  var i=1;
+  {  i=1;
+     total_week = 0;
      while (i <= 7){
-       calculate_hours(i,index);
+       total_week = total_week + parseInt(calculate_hours(i,index));
        i++;
       }
+    $("#tr-"+index+" #total_week").text(convert_time_msecond(total_week) );
   })
 }
 function calculate_hours(day,index){
-  var text_on = $("#tr-"+index+" #"+day+"-on").text(),
+  var h_on,m_on,s_on,h_off,m_off,s_off,
+      day_on = new Date(),
+      day_off = new Date(),
+      text_on = $("#tr-"+index+" #"+day+"-on").text(),
       text_off = $("#tr-"+index+" #"+day+"-off").text();
+    //  text_on = $("#tr-"+index+" #"+day+"-on input") ? $("#tr-"+index+" #"+day+"-on input").val() : $("#tr-"+index+" #"+day+"-on").text(),
+    //  text_off =  $("#tr-"+index+" #"+day+"-off input") ? $("#tr-"+index+" #"+day+"-off input").val() : $("#tr-"+index+" #"+day+"-off").text();
 
-  //function restarHoras(inicio,fin) {
+  //   text_on = ''+text_on+''
+  //   text_off = ''+text_off+''
+     h_on = text_on.substr(0, 2);
+    m_on = text_on.substr(3, 2);
+    h_off = text_off.substr(0, 2);
+    m_off = text_off.substr(3, 2);
 
-    var day_on = new Date(),
-        day_off = new Date();
-    text_on = text_on.split(":");
-    text_off = text_off.split(":");
-    day_on.setHours(text_on[0], text_on[1].split[" "], 0);
-    day_off.setHours(text_off[0], text_off[1].split[" "], 0);
-    var dif= day_off - day_on; // diferencia en milisegundos
+    day_on.setHours(h_on, m_on, 0);
+    day_off.setHours(h_off, m_off, 0);
+    var diff = day_off - day_on;
+//diff_time(day_on, day_off)
+    $("#tr-"+index+" #"+day+"-total").text(convert_time_msecond(diff));
+    return diff;
 
-    var difSeg = Math.floor(dif/1000); //diferencia en segundos
+}
 
-    var segundos = difSeg % 60; //segundos
+function convert_time_msecond(msecond){
+    var msecPerMinute = 1000 * 60;
+    var msecPerHour = msecPerMinute * 60;
+    var msecPerDay = msecPerHour * 24;
 
-    var difMin = Math.floor(difSeg/60); //diferencia en minutos
+    // Get the difference in milliseconds.
 
-    var minutos = difMin % 60; //minutos
+    // Calculate how many days the dif contains. Subtract that
+    var days = Math.floor(msecond / msecPerDay );
+    msecond = msecond - (days * msecPerDay );
+    // Calculate the hours, minutes, and seconds.
+    var hours = Math.floor(msecond / msecPerHour );
+    msecond = msecond - (hours * msecPerHour );
 
-    var difHs = Math.floor(difMin/60); //diferencia en horas
+    var minutes = Math.floor(msecond / msecPerMinute );
+    msecond = msecond - (minutes * msecPerMinute );
 
-    var horas = difHs % 24; //horas
+    var seconds = Math.floor(msecond / 1000 );
 
-    var total= horas+":"+minutos+":"+segundos; //armo el tiempo de diferencia
-   $("#tr-"+index+" #"+day+"-total").text(total);
-  //  }
-
+    return (hours < 10 ? "0" + hours : hours) + ":" + (minutes < 10 ? "0" + minutes : minutes) ;
 }
