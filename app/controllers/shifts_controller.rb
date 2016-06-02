ba54a6ead6d534d12ad2db2df7320124a84f5e4a
admin <admin@iMac-2.local>
2016-06-02 17:45:56 -0400

    Changes:1. Total hours to decimal format 2.Select all sites

--- app/controllers/shifts_controller.rb
+++ app/controllers/shifts_controller.rb
@@ -5,34 +5,76 @@ class ShiftsController < ApplicationController
   # Functions looking for Guards and their week shedule
   def found
     date = params[:date].to_time
-    site = params[:site]
+    # site = params[:site]
     week = []
     day_0 = date.strftime('%d').to_i - (date.strftime('%w').to_i-1)
     date_m = (date.strftime('%Y-')+date.strftime('%m-')+day_0.to_s).to_time
     if Shift.all.length != 0
-      site_id = Site.find_by(codename: params[:site]).id
-      shifts_by_date = Shift.where(:datetime => Time.at(date_m)..Time.at(date_m) + 8.days, :site_id => site_id)
+      if params[:site]!= ""
+        site_id = Site.find_by(codename: params[:site]).id
+        shifts_by_date = Shift.where(:datetime => Time.at(date_m)..Time.at(date_m) + 8.days, :site_id => site_id)
+      else
+        shifts_by_date = Shift.where(:datetime => Time.at(date_m)..Time.at(date_m) + 8.days)
+      end # END site!= ""
       if shifts_by_date.all.length != 0
-        guards = shifts_by_date.select("guard_id").uniq
-        week_number = date_m.strftime('%W');
+        sites = shifts_by_date.select("site_id").uniq
         count = 0
-        guards.each do |guard|
-          week_per_guard = find_shifts(guard.guard_id, week_number, shifts_by_date)
-          if week_per_guard != ""
-            week[count] = week_per_guard
-            count += 1
-          end
-        end # END guards.each
-
-      end # END shifts_by_date.length
+        sites.each do |site|
+          guards = shifts_by_date.select(:guard_id).uniq { |m| m.site_id  == site.site_id }
+          week_number = date_m.strftime('%W');
+          guards.each do |guard|
+            all_shifts = shifts_by_date.where(guard_id: guard.guard_id, site_id: site.site_id)
+            week_per_guard = find_shifts(guard.guard_id, week_number, all_shifts)
+            if week_per_guard != ""
+              week[count] = week_per_guard
+              count += 1
+            end
+           end # END guards.each
+         end # END sites.each
+       end # END shifts_by_date.length
     end # END  Shift.length != 0
+  # def found
+  #   date = params[:date].to_time
+  #   site = params[:site]
+  #   week = []
+  #   day_0 = date.strftime('%d').to_i - (date.strftime('%w').to_i-1)
+  #   date_m = (date.strftime('%Y-')+date.strftime('%m-')+day_0.to_s).to_time
+  #   if Shift.all.length != 0
+  #     if site!= ""
+  #       site_id = Site.find_by(codename: params[:site]).id
+  #       shifts_by_date = Shift.where(:datetime => Time.at(date_m)..Time.at(date_m) + 8.days, :site_id => site_id)
+  #       if shifts_by_date.all.length != 0
+  #         guards = shifts_by_date.select("guard_id").uniq
+  #         week_number = date_m.strftime('%W');
+  #         count = 0
+  #         guards.each do |guard|
+  #           week_per_guard = find_shifts(guard.guard_id, week_number, shifts_by_date)
+  #           if week_per_guard != ""
+  #             week[count] = week_per_guard
+  #             count += 1
+  #           end
+  #         end # END guards.each
+  #       end # END shifts_by_date.length
+  #     else
+  #       shifts_by_date = Shift.where(:datetime => Time.at(date_m)..Time.at(date_m) + 8.days)
+  #       if shifts_by_date.all.length != 0
+  #         sites = shifts_by_date.select("site_id").uniq
+  #         sites.each do |site|
+  #           week_per_guard = find_shifts(guard.guard_id, week_number, shifts_by_date)
+  #           if week_per_guard != ""
+  #             week[count] = week_per_guard
+  #             count += 1
+  #           end
+  #         end # END sites.each
+  #       end # END shifts_by_date.all.length != 0
+  #     end # END site!= ""
+  #   end # END  Shift.length != 0
     respond_to do |format|
       format.html { redirect_to shifts_path, notice: 'Shifts_by_date was successfully created.' }
       format.json { render json: week }
     end # respond_to
  end
- def find_shifts(guard, week_number,shifts_by_date)
-   all_shifts = shifts_by_date.where(guard_id: guard)
+ def find_shifts(guard, week_number,all_shifts)
    all_shifts = all_shifts.sort_by{|t| t.datetime}
    flag = false
    week = Hash.new
@@ -45,7 +87,6 @@ class ShiftsController < ApplicationController
          time = time_round(shift.datetime.strftime('%H:%M'))
          d_time = Time.new(0,1,1,shift.datetime.strftime('%H').to_i,shift.datetime.strftime('%M').to_i)
          d_noon = Time.new(0,1,1,12,0)
-
          flag_before = false
          # THIS IF CHECK IF THE SHIFT IS BELONG TO THE CURRENT WEEK
          if shift_week_num == week_number
@@ -73,7 +114,6 @@ class ShiftsController < ApplicationController
             end
          end   # IF END
      end  # all_shifts.each do |shift| END
-
    end # IF all_shifts.length != 0
    if week.length != 0
      week['guard'] = Guard.find(guard).first_name + " " + Guard.find(guard).last_name
@@ -82,17 +122,14 @@ class ShiftsController < ApplicationController
    else
      return ""
    end
-
   end   # def END
   def time_round(time)
     time = time.split(":")
     hours = time[0].to_i
     min = time[1].to_i
-
     if min >=51 || min <=5
       if min >= 51 && min <= 59
-
-         hours =  (hours < 23)? hours + 1: 0
+        hours =  (hours < 23)? hours + 1: 0
       end
       min  = 0
     elsif min >= 6 && min <= 20
@@ -102,11 +139,9 @@ class ShiftsController < ApplicationController
     elsif min >= 36 && min <= 50
       min = 45
     end
-
     hours = hours < 10 ? "0" + hours.to_s : hours.to_s
     min = min < 10 ? "0" + min.to_s : min.to_s
     return  hours + ":" + min
-
   end  # def END
  # END Functions looking for Guards and their week shedule
   def name_site
@@ -115,13 +150,9 @@ class ShiftsController < ApplicationController
   # GET /shifts
   # GET /shifts.json
   def index
-
     @shifts = Shift.all
-
     @guards = Guard.all
-
     @shift = Shift.new
-
     respond_to do |format|
       format.html
       format.xls
@@ -133,36 +164,25 @@ class ShiftsController < ApplicationController
         render_to_string('shifts/index.html.erb', layout: false))
       end
     end
-
   end
-
   # GET /shifts/1
   # GET /shifts/1.json
   def show
   end
-
   # GET /shifts/new
   def new
-
     @shift = Shift.new
-
   end
-
   # GET /shifts/1/edit
   def edit
   end
-
   # POST /shifts
   # POST /shifts.json
   def create
-
   #import redirects to import method in this(shifts) controller
     import
     @shift = Shift.new(shift_params)
-
   end
-
-
   # PATCH/PUT /shifts/1
   # PATCH/PUT /shifts/1.json
   def update
@@ -177,21 +197,17 @@ class ShiftsController < ApplicationController
       end
     end
   end
-
   # DELETE /shifts/1
   # DELETE /shifts/1.json
   def destroy
     @shift.destroy
-
     respond_to do |format|
       format.html { redirect_to shifts_url, notice: 'Shift was successfully destroyed.' }
       format.json { head :no_content }
       Shift.reset_pk_sequence
     end
   end
-
   def remove_all
-
     Shift.delete_all
     Shift.reset_pk_sequence
     Guard.delete_all
@@ -200,25 +216,18 @@ class ShiftsController < ApplicationController
     Site.reset_pk_sequence
     flash[:notice] = "All shift data has been cleared!"
     redirect_to shifts_path
-
   end
-
   def import
     Shift.import(params[:file])
       redirect_to shifts_path, notice: "Shifts uploaded succesfully"
   end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_shift
-
       @shift = Shift.find(params[:id])
     end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def shift_params
-
       params.fetch(:shift, {}).permit(:file, :search_date)
-
     end
 end
