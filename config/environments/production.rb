fae0b520e764bdeab7cd277643b7c7df0a31f622
Santiago Garza <sgarzaceja@me.com>
2016-07-08 16:52:37 -0400

    changed url to startend

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
 
   #Configure mailer through sendgrid
 
-  config.action_mailer.default_url_options = { :host => 'standupsessions.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'startend.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
